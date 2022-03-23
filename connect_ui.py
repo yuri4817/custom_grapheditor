43d1a79b70a0b58ee7724a2594d3421b8a73293e
Yuri Sato <heiwayuuri@gmail.com>
2022-03-23 11:27:32 +0900

    Update UI
    add translate button

--- connect_ui.py
+++ connect_ui.py
@@ -5,10 +5,42 @@ from PySide2.QtUiTools import QUiLoader
 import functools
 
 
+def graphEditorOutlineEds():
+    graph_panels = set()
+    for panel in cmds.getPanel(type='scriptedPanel') or []:
+        if cmds.scriptedPanel(panel, query=True, type=True) != 'graphEditor':
+            continue
+        graph_panels.add(panel)
+    outliner_editors = set()
+    for editor in cmds.lsUI(editors=True) or []:
+        if not cmds.outlinerEditor(editor, exists=True):
+            continue
+        if cmds.outlinerEditor(editor, q=True, masterOutliner=True):
+            continue
+        if cmds.outlinerEditor(editor, q=True, panel=True) not in graph_panels:
+            continue
+        outliner_editors.add(editor)
+    return list(outliner_editors)
+
+
+
+def graphEditorFilterAttributes(attrs, values):
+    if len(attrs) != len(values):
+        raise ValueError
+    
+    cmd = ''
+    for editor in graphEditorOutlineEds():
+        for attr, value in zip(attrs, values):
+            cmd += 'filterUISelectAttributesCheckbox {} {:d} {};'.format(attr, value, editor)
+    mel.eval(cmd)  
+
+#graphEditorFilterAttributes(['translateX', 'rotateZ', 'scaleY'], [True, False, False])
+
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.ui = QUiLoader().load("D:\\MAC_Demo\\resources\\tools\\maya\\python\\graph_editor\\graphEditorFilterAttributes.ui")
+        self.ui = QUiLoader().load("D:\\graphEditorFilterAttributes.ui")
         self.setCentralWidget(self.ui)
         for op in ('translate', 'rotate', 'scale'):
             for axis in ('X', 'Y', 'Z'): 
@@ -17,10 +49,47 @@ class MainWindow(QMainWindow):
                 # obj.toggled.connect(functools.partial(
                 #     graphEditorFilterAttribute,name))
                 obj.toggled.connect(lambda value, name_ = name:graphEditorFilterAttributes([name_], [value]))
+        self.ui.visibility.toggled.connect(graphEditorFilterAttributes)
+        #self.togglebutton()
+        self.ui.translate.clicked.connect(self.togglebutton)
+        #print(self.togglebutton)
+
+    def togglebutton(self):
+        bool_list = []
+        for op in ('translate',):
+            # test = all((getattr(self.ui, op + axis).isChecked() for axis in ('X', 'Y', 'Z')))
+            # #print(test)
+            for axis in ('X', 'Y', 'Z'): 
+                name = op + axis  
+                obj = getattr(self.ui, name)
+                #print(obj)
+                obj.isChecked()
+                #print(obj.isChecked())
+                #return "haichu"
+                bool_list.append(obj.isChecked())
+        if  all(bool_list) == True:
+            #print("pika")
+            
+            for op in ('translate',):
+                for axis in ('X', 'Y', 'Z'): 
+                    name = op + axis  
+                    obj = getattr(self.ui, name)
+                    #print(obj)
+                    obj.setChecked(False)
+            
+        else:
+            #print("raichu")
+            for op in ('translate',):
+                for axis in ('X', 'Y', 'Z'): 
+                    name = op + axis  
+                    obj = getattr(self.ui, name)
+                    obj.setChecked(True)
 
 
-         self.ui.visibility.toggled.connect(graphEditorFilterAttribute)
- 
+
+        
+
+        
     
     # def graphEditorFilterAttributes(self):
     #     values = []
