d9e48ae4652ca0a8fb5574a7d41f44ac9a533681
Yuri Sato <heiwayuuri@gmail.com>
2022-04-01 09:37:33 +0900

    update UI

--- PoseOffset.py
+++ PoseOffset.py
@@ -1,45 +1,58 @@
 import maya.cmds as cmds
-
-#get curve attributes
-curves_attrs = cmds.listConnections(t='animCurve', c=True, p=True, d=False)
-input_attrs = curves_attrs[0::2]
-
-for input_ in input_attrs:
-    node = input_.split(".")[0]
-    attr = ".".join(input_.split(".")[1:])  
-
-    time_ = cmds.currentTime( query=True )
-    key_val =cmds.keyframe(node,at=attr,t=(time_,time_),q=True,eval=True)
-    #print(key_val)
-    ch_val = [cmds.getAttr(input_)]
-    #print(input_, ch_val)
-
-    #keyフレームの値とチャンネルボックスの値が違うのだけプリント
-    if key_val != ch_val:
-        print(input_,key_val,ch_val)
-        diff_val = ch_val[0] - key_val[0]
-        #print(diff_val)
-
-        #違う値の差分とどのアトリビュートかをだして、その値を使ってカーブをオフセット
-        cmds.keyframe(at=attr, o="move", r=True, vc=diff_val)
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtUiTools import QUiLoader
 
 
+def poseOffset():
+    curves_attrs = cmds.listConnections(t='animCurve', c=True, p=True, d=False)
+    input_attrs = curves_attrs[0::2]
+
+    for input_ in input_attrs:
+        node = input_.split(".")[0]
+        attr = ".".join(input_.split(".")[1:])  
+
+        time_ = cmds.currentTime( query=True )
+        key_val =cmds.keyframe(node,at=attr,t=(time_,time_),q=True,eval=True)
+        ch_val = [cmds.getAttr(input_)]
+
+        #keyフレームの値とチャンネルボックスの値が違うのだけプリント
+        if key_val != ch_val:
+            print(input_,key_val,ch_val)
+            diff_val = ch_val[0] - key_val[0]
+            #print(diff_val)
+
+            #違う値の差分とどのアトリビュートかをだして、その値を使ってカーブをオフセット
+            cmds.keyframe(at=attr, o="move", r=True, vc=diff_val)
 
 
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        poseOffset()
+        self.ui = QUiLoader().load("D:\\github\\CustomEditor\\poseoffset.ui")
+        self.setCentralWidget(self.ui)
+    #     for op in ('translate', 'rotate', 'scale'):
+    #         for axis in ('X', 'Y', 'Z'): 
+    #             name = op + axis  
+    #             obj = getattr(self.ui, name)
+    #             obj.toggled.connect(lambda value, name_=name: graphEditorFilterAttributes([name_], [value]))
+    #     self.ui.translate.clicked.connect(lambda: self.togglebutton('translate'))
+        
 
-#Mistake
-# value_ = cmds.getAttr('pCube1_translateY',time=1)
-# print(value_)
-# object = "pCube1"
-# attr = "ty"
-
-# value_ =cmds.keyframe(object + "." + attr)
-# print(value_)
-
-#cmds.scaleKey(curve[2], valuePivot=0, valueScale=10 ) 
-#input_attrs = [attrs for attrs in curves_attrs if attrs % 2 != 0]
-
-#print(set(key_val) ^ set(ch_val))
-
+    # def togglebutton(self, op):
+    #     value = not all((getattr(self.ui, op + axis).isChecked() for axis in ('X', 'Y', 'Z')))
+    #     for axis in ('X', 'Y', 'Z'): 
+    #         name = op + axis  
+    #         obj = getattr(self.ui, name)
+    #         blocked = obj.blockSignals(True)
+    #         obj.setChecked(value)
+    #         obj.blockSignals(blocked)
+        
+    #     names = [op + 'X', op + 'Y', op + 'Z']
+    #     values = [value, value, value]
+    #     graphEditorFilterAttributes(names, values)
 
 
